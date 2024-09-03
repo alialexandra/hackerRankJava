b755245b1ab6581ad77ca5bd2f15953a421ea088
alialexandra <alialexandra_94@yahoo.com>
2024-09-03 22:28:47 +0200

    some exercises

---
+++ src/com/company/java/intermediate/regex/Solution.java
@@ -0,0 +1,7 @@
+package com.company.java.intermediate.regex;
+
+public class Solution {
+
+    String pattern = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+
+}
